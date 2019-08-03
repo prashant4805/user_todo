64b3d5dc11d566ea42a1864529bbbff2b99bc439
dell <dell@DESKTOP-GJ3OR1F>
2019-08-03 21:16:52 +0530

    First Commit
    Uploading User Todo Api

---
+++ usertodoapi/src/main/java/com/spring/usertodoapi/UsertodoapiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.usertodoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsertodoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsertodoapiApplication.class, args);
+	}
+
+}
